978ea6b5c55882ea77d1315b08803bef5924eb62
Jonathan Stull <jonathan.d.stull@gmail.com>
2021-06-11 15:47:09 -0700

    updates services to handle user CRUD functionality

--- NationalParkSystem/Services/UserService.cs
+++ NationalParkSystem/Services/UserService.cs
@@ -1,59 +1,123 @@
+using AutoMapper;
+using BCryptNew = BCrypt.Net.BCrypt;
+using System.Collections.Generic;
+using System.Linq;
+using NationalParkSystem.Authorization;
+using NationalParkSystem.Entities;
+using NationalParkSystem.Helpers;
+using NationalParkSystem.Models.Users;
+
 using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
 using System;
-using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
-using System.Linq;
 using System.Security.Claims;
 using System.Text;
-using NationalParkSystem.Entities;
-using NationalParkSystem.Helpers;
-using NationalParkSystem.Models;
 
 namespace NationalParkSystem.Services
 {
   public interface IUserService
   {
     AuthenticateResponse Authenticate(AuthenticateRequest model);
+    IEnumerable<User> GetAll();
+    User GetById(int id);
+    void Register(RegisterRequest model);
+    void Update(int id, UpdateRequest model);
+    void Delete(int id);
   }
 
   public class UserService : IUserService
   {
-    private List<User> _users = new List<User>
-    {
-      new User { Id = 1, FirstName = "Jonathan", LastName = "Stull", Username = "Jonathan", Password = "jonathan" }         // hard-coded user; this implementation doesn't handle user registration
-    };
+    private DuplicateTypeMapConfigurationException _context;
+    private IJwtUtils _jwtUtils;
+    private readonly IMapper _mapper;
 
-    private readonly AppSettings _appSettings;                                                                              // appsettings property; correlated with AppSettings class, which has property Secret
+    // private readonly AppSettings _appSettings;                                                                           // appsettings property; correlated with AppSettings class, which has property Secret
 
-    public UserService(IOptions<AppSettings> appSettings)                                                                   // instantiates class with user secret in appsettings.json
+    public UserService(DataContext context, IJwtUtils jwtUtils, IMapper mapper)                                             // instantiates class with user secret in appsettings.json
     {
-      _appSettings = appSettings.Value;
+      _context = context;
+      _jwtUtils = jwtUtils;
+      _mapper = mapper;
     }
 
     public AuthenticateResponse Authenticate(AuthenticateRequest model)                                                     // custom authentication method
     {
-      var user = _users.SingleOrDefault(x => x.Username == model.Username && x.Password == model.Password);                 // validates user with username and password
+      var user = _context.Users.SingleOrDefault(x => x.Username == model.Username);                                         // calls the user to the current context
 
-      if (user == null) return null;                                                                                        // returns nothing if no input
+      if (user == null) || !BCryptNet.Verify(model.Password, user.PasswordHash);                                            // validates
+          throw new ApplicationException("Username or password is incorrect");
 
-      var token = generateJwtToken(user);                                                                                   // calls custom method to generate JWT
+      var response = _mapper.Map<AuthenticateResponse>(user);
+      response.JwtToken = _jwtUtils.GenerateToken(user);                                                                    // calls custom method to generate JWT
 
-      return new AuthenticateResponse(user, token);                                                                         // returns the user and the token in the response
+      return response;                                                                                                      // returns the user and the token in the response
     }
 
-    private string generateJwtToken(User user)                                                                              // generates the JWT token, which remains active for 7 days
+    public IEnumerable<User> GetAll()
     {
-      var tokenHandler = new JwtSecurityTokenHandler();                                                                     // instantiates a token handler
-      var key = Encoding.ASCII.GetBytes(_appSettings.Secret);                                                               // generates array of bytes converted from the secret in appsettings.json
-      var tokenDescriptor = new SecurityTokenDescriptor
-      {
-        Subject = new ClaimsIdentity(new[] { new Claim("id", user.Id.ToString()) }),                                        // instatiates a user claim for this user
-        Expires = DateTime.UtcNow.AddDays(7),                                                                               // sets an expiration for the JWT
-        SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)  // generates security key based on the converted secret
-      };
-      var token = tokenHandler.CreateToken(tokenDescriptor);                                                                // creates the JWT token with required attributes
-      return tokenHandler.WriteToken(token);                                                                                // returns the token to the authentication method
+      return _context.Users;
     }
+
+    public User GetById(int id)
+    {
+      return getUser(id);
+    }
+
+    public void Register(RegisterRequest model)
+    {
+      if (_context.Users.Any(x => x.Username == model.Username))
+          throw new ApplicationException("Username '" + model.Username + "' is already taken");                             // checks to see if username already exists
+    
+      var user = _mapper.Map<User>(model);
+
+      user.PasswordHash = BCryptNet.HashPassword(model.Password);
+
+      _context.Users.Add(user);
+      _context.SaveChanges();
+    }
+
+    public void Update(int id, UpdateRequest model)
+    {
+      var user = getUser(id);
+
+      if (model.Username != user.Username && _context.Users.Any(x => x.Username == model.Username))
+          throw new ApplicationException("Username '" + model.Username + "' is already taken");
+
+      if (!string.IsNullOrEmpty(model.Password))
+          user.PasswordHash = BCryptNet.HashPassword(model.Password);
+
+      _mapper.Map(model, user);
+      _context.Users.Update(user);
+      _context.SaveChanges();
+    }
+
+    public void Delete(int id)
+    {
+      var user = getUser(id);
+      _context.Users.Remove(user);
+      _context.SaveChanges();
+    }
+
+    private User getUser(int id)
+    {
+      var user = _context.Users.Find(id);
+      if (user == null) throw new KeyNotFoundException("User not found");
+      return user;
+    }
+
+    // private string generateJwtToken(User user)                                                                              // generates the JWT token, which remains active for 7 days
+    // {
+    //   var tokenHandler = new JwtSecurityTokenHandler();                                                                     // instantiates a token handler
+    //   var key = Encoding.ASCII.GetBytes(_appSettings.Secret);                                                               // generates array of bytes converted from the secret in appsettings.json
+    //   var tokenDescriptor = new SecurityTokenDescriptor
+    //   {
+    //     Subject = new ClaimsIdentity(new[] { new Claim("id", user.Id.ToString()) }),                                        // instatiates a user claim for this user
+    //     Expires = DateTime.UtcNow.AddDays(7),                                                                               // sets an expiration for the JWT
+    //     SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)  // generates security key based on the converted secret
+    //   };
+    //   var token = tokenHandler.CreateToken(tokenDescriptor);                                                                // creates the JWT token with required attributes
+    //   return tokenHandler.WriteToken(token);                                                                                // returns the token to the authentication method
+    // }
   }
 }
