6a62f9f3b1f993111c5aeb610c60462df8c3afbc
Jonathan Stull <jonathan.d.stull@gmail.com>
2021-06-11 13:40:10 -0700

    removes unused utility methods and adds commented explanations

--- NationalParkSystem/Services/UserService.cs
+++ NationalParkSystem/Services/UserService.cs
@@ -15,57 +15,45 @@ namespace NationalParkSystem.Services
   public interface IUserService
   {
     AuthenticateResponse Authenticate(AuthenticateRequest model);
-    IEnumerable<User> GetAll();
-    User GetById(int id);
   }
 
   public class UserService : IUserService
   {
     private List<User> _users = new List<User>
     {
-      new User { Id = 1, FirstName = "Jonathan", LastName = "Stull", Username = "Jonathan", Password = "jonathan" }
+      new User { Id = 1, FirstName = "Jonathan", LastName = "Stull", Username = "Jonathan", Password = "jonathan" }         // hard-coded user; this implementation doesn't handle user registration
     };
 
-    private readonly AppSettings _appSettings;
+    private readonly AppSettings _appSettings;                                                                              // appsettings property; correlated with AppSettings class, which has property Secret
 
-    public UserService(IOptions<AppSettings> appSettings)
+    public UserService(IOptions<AppSettings> appSettings)                                                                   // instantiates class with user secret in appsettings.json
     {
       _appSettings = appSettings.Value;
     }
 
-    public AuthenticateResponse Authenticate(AuthenticateRequest model)
+    public AuthenticateResponse Authenticate(AuthenticateRequest model)                                                     // custom authentication method
     {
-      var user = _users.SingleOrDefault(x => x.Username == model.Username && x.Password == model.Password);
+      var user = _users.SingleOrDefault(x => x.Username == model.Username && x.Password == model.Password);                 // validates user with username and password
 
-      if (user == null) return null;
+      if (user == null) return null;                                                                                        // returns nothing if no input
 
-      var token = generateJwtToken(user);
+      var token = generateJwtToken(user);                                                                                   // calls custom method to generate JWT
 
-      return new AuthenticateResponse(user, token);
+      return new AuthenticateResponse(user, token);                                                                         // returns the user and the token in the response
     }
 
-    public IEnumerable<User> GetAll()
+    private string generateJwtToken(User user)                                                                              // generates the JWT token, which remains active for 7 days
     {
-      return _users;
-    }
-
-    public User GetById(int id)
-    {
-      return _users.FirstOrDefault(x => x.Id == id);
-    }
-
-    private string generateJwtToken(User user)
-    {
-      var tokenHandler = new JwtSecurityTokenHandler();
-      var key = Encoding.ASCII.GetBytes(_appSettings.Secret);
+      var tokenHandler = new JwtSecurityTokenHandler();                                                                     // instantiates a token handler
+      var key = Encoding.ASCII.GetBytes(_appSettings.Secret);                                                               // generates array of bytes converted from the secret in appsettings.json
       var tokenDescriptor = new SecurityTokenDescriptor
       {
-        Subject = new ClaimsIdentity(new[] { new Claim("id", user.Id.ToString()) }),
-        Expires = DateTime.UtcNow.AddDays(7),
-        SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+        Subject = new ClaimsIdentity(new[] { new Claim("id", user.Id.ToString()) }),                                        // instatiates a user claim for this user
+        Expires = DateTime.UtcNow.AddDays(7),                                                                               // sets an expiration for the JWT
+        SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)  // generates security key based on the converted secret
       };
-      var token = tokenHandler.CreateToken(tokenDescriptor);
-      return tokenHandler.WriteToken(token);
+      var token = tokenHandler.CreateToken(tokenDescriptor);                                                                // creates the JWT token with required attributes
+      return tokenHandler.WriteToken(token);                                                                                // returns the token to the authentication method
     }
   }
 }
