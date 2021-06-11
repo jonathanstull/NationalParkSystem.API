8eba580691c1534963e2e569c250254d8b478b48
Jonathan Stull <jonathan.d.stull@gmail.com>
2021-06-11 10:17:02 -0700

    fixes property protection level

--- NationalParkSystem/Models/StatePark.cs
+++ NationalParkSystem/Models/StatePark.cs
@@ -4,34 +4,34 @@ namespace NationalParkSystem.Models
 {
   public class StatePark
   {
-    int StateParkId { get; set; }
+    public int StateParkId { get; set; }
     
     [Required]
-    string Name { get; set; }
+    public string Name { get; set; }
 
     [Required]
-    string Status { get; set; } // lists federal or state designation (e.g., national monument, state historic site, etc.)
+    public string Status { get; set; } // lists federal or state designation (e.g., national monument, state historic site, etc.)
 
     [Required]
     [StringLength(22, ErrorMessage = "GPS coordinates must be in this format: 45.516022, -122.609424")]
-    string LatLong { get; set; } // specifies the coordinates of the park visitor center, if any; if more than one, the most centrally located is used, and if none, the main park access point will be used
+    public string LatLong { get; set; } // specifies the coordinates of the park visitor center, if any; if more than one, the most centrally located is used, and if none, the main park access point will be used
 
     [Required]
     [StringLength(2, ErrorMessage = "State code must in this format: OR")]
-    string State { get; set; } // state in which the park resides
+    public string State { get; set; } // state in which the park resides
 
     [Required]
-    int Visits { get; set; } // specifies annual visitation where most recent data exists
+    public int Visits { get; set; } // specifies annual visitation where most recent data exists
 
     [Required]
-    string BusySeason { get; set; }
+    public string BusySeason { get; set; }
     
     [Required]
-    string Climate { get; set; } // specifies the USDA climate zone in which the park lies
+    public string Climate { get; set; } // specifies the USDA climate zone in which the park lies
 
     [Required]
-    bool RvServices { get; set; } // for electrical/sewage services; also reflects general park vehicle accessibility
+    public bool RvServices { get; set; } // for electrical/sewage services; also reflects general park vehicle accessibility
 
-    blob Topo { get; set; }  // stretch goal: implement topographical map data in API calls
+    public byte[] Topo { get; set; }  // stretch goal: implement topographical map data in API calls
   }
 }
